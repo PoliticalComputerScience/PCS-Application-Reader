2d65f2d66dc763ce15692c8d94c004656985943d
theArctesian <stephen.d.okita@gmail.com>
2024-01-25 18:08:33 -0800

    init need to add sec features

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,10 @@
-import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
-	kit: {
-		adapter: vercel(),
-	}
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
 };
-
 export default config;
